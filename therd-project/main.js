c597f270fa1a3f713ceac131b1084036e9c73aad
Yosef mare <yosefmare108@gmail.com>
2023-07-04 18:44:31 +0300

    create the calculation logic

--- therd-project/main.js
+++ therd-project/main.js
@@ -1,7 +1,21 @@
 const btns = document.getElementsByClassName("btn");
 const screen = document.getElementById("screen");
-for(let i = 0; i < btns.length; i++){
-    btns[i].onclick = function(){
-        screen.value += btns[i].textContent
+const equal = document.getElementById("equal");
+const reset = document.getElementById("reset");
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].onclick = function () {
+        let numberStringType = String(btns[i].textContent)
+        screen.value += numberStringType
+        console.log(screen.value);
+        let calculation = eval(screen.value)
+        equal.onclick = function(){
+            screen.value = calculation
+        }
+
+        reset.onclick = function(){
+            screen.value = ""
+        }
     }
 }
