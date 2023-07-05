b3c44d4592f4871ac6ed7bc016bd408fd474e3d8
Yosef mare <yosefmare108@gmail.com>
2023-07-05 11:15:38 +0300

    create the del button to delete numbers

--- therd-project/main.js
+++ therd-project/main.js
@@ -2,20 +2,26 @@ const btns = document.getElementsByClassName("btn");
 const screen = document.getElementById("screen");
 const equal = document.getElementById("equal");
 const reset = document.getElementById("reset");
-
+const deleteNumberByNumber = document.getElementById("delete");
+let arr = [];
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
-        let numberStringType = String(btns[i].textContent)
-        screen.value += numberStringType
-        console.log(screen.value);
-        let calculation = eval(screen.value)
-        equal.onclick = function(){
-            screen.value = calculation
-        }
+        let numberStringType = String(btns[i].textContent);
+        screen.value += numberStringType;
+        arr.push(numberStringType);
+        let calculation = eval(screen.value);
 
-        reset.onclick = function(){
-            screen.value = ""
-        }
-    }
+        equal.onclick = function() {
+            screen.value = calculation;
+        };
+
+        reset.onclick = function() {
+            screen.value = "";
+        };
+
+        deleteNumberByNumber.onclick = function() {
+            screen.value = screen.value.slice(0, -1);
+        };
+    };
 }
